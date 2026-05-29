b38bede572e69235bcc9ef01d3aa7f6f7d6ae38f
DylanP35 <dperren0002@mymail.lausd.net>
2026-05-29 10:40:09 -0700

    added coded

--- src/main/java/com/myRobot/FinalFinch.java
+++ src/main/java/com/myRobot/FinalFinch.java
@@ -7,8 +7,6 @@ public class FinalFinch extends Finch {
 public FinalFinch(String device){
 super (device);
 }
-public void test(){
-    bird.setMove("F", 10, 50);
-}
+
 
 }
--- src/main/java/com/myRobot/TesterFinch.java
+++ src/main/java/com/myRobot/TesterFinch.java
@@ -4,6 +4,18 @@ public class TesterFinch{
 public static void main(String[]args){
 
 FinalFinch bird = new FinalFinch("A");
-bird.test();
+    // bird.setMotors(-50, 50);
+    
+    bird.setMove("F", 60, 50);
+    bird.setTurn("L", 90, 50);
+    bird.setMove("F", 20, 50);
+    bird.setTurn("L", 90, 50);
+    bird.setMove("F", 60, 50);
+    bird.setTurn("R", 90, 50);
+    bird.setMove("F", 60, 50);
+    bird.setTurn("L", 90, 50);
+    bird.setMove("F", 60, 50);
+
+    bird.stop();
 }
 }

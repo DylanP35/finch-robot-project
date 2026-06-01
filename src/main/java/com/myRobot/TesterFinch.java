39d675142d5641fa4ab494b04f687b0bc5fab610
DylanP35 <dperren0002@mymail.lausd.net>
2026-06-01 10:37:17 -0700

    LA LOGO

--- src/main/java/com/myRobot/TesterFinch.java
+++ src/main/java/com/myRobot/TesterFinch.java
@@ -4,17 +4,40 @@ public class TesterFinch{
 public static void main(String[]args){
 
 FinalFinch bird = new FinalFinch("A");
-    // bird.setMotors(-50, 50);
     
-    bird.setMove("F", 60, 50);
-    bird.setTurn("L", 90, 50);
+bird.setMove("F", 10, 50);
+bird.setMove("B", 5, 50);
+    bird.setTurn("R", 90, 50);
     bird.setMove("F", 20, 50);
     bird.setTurn("L", 90, 50);
-    bird.setMove("F", 60, 50);
-    bird.setTurn("R", 90, 50);
-    bird.setMove("F", 60, 50);
+    bird.setMove("B", 3, 50);
+    bird.setMove("F", 20, 50);
     bird.setTurn("L", 90, 50);
-    bird.setMove("F", 60, 50);
+    bird.setMove("F", 3, 50);
+    bird.setMove("B", 3, 50);
+    bird.setTurn("L", 90, 50);
+    bird.setMove("F", 13, 50);
+    bird.setTurn("L", 45, 50);
+    bird.setMove("F", 3, 50);
+    bird.setTurn("R", 45, 50);
+    bird.setMove("F", 1.5, 50);
+    bird.setMove("B", 3, 50);
+    bird.setMove("F", 1.5, 50);
+    bird.setTurn("R", 120, 50);
+    bird.setMove("F", 15, 50);
+    bird.setTurn("R", 70, 50);
+    bird.setTurn("R", 60, 50);
+    bird.setMove("F", 12, 50);
+    bird.setTurn("R", 15, 50);
+    bird.setMove("F", 2.5, 50);
+    bird.setTurn("L", 90, 50);
+    bird.setMove("F", 1.5, 50);
+    bird.setMove("B", 3, 50);
+
+    
+
+
+
 
     bird.stop();
 }
